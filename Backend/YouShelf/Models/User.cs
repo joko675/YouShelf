d9507e7c642b51faf94731e078ad06738833bc4e
joko675 <128475080+joko675@users.noreply.github.com>
2026-03-21 17:33:15 +0100

    Add files via upload

---
+++ Backend/YouShelf/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace YouShelf.Models;
+
+public class User
+{
+    public string Username { get; set; }
+    public string PasswordHash { get; set; }
+}
---
+++ Backend/YouShelf/Program.cs
@@ -0,0 +1,60 @@
+using dotenv.net;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.Data.Sqlite;
+using Microsoft.IdentityModel.Tokens;
+using System.Data;
+using System.Text;
+using YouShelf.Repositories;
+using YouShelf.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+DotEnv.Load();
+
+// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(jwtOptions =>
+{
+    jwtOptions.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER"),
+
+        ValidateAudience = true,
+        ValidAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE"),
+
+        ValidateLifetime = true,
+
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_SECRET")))
+    };
+    jwtOptions.MapInboundClaims = false;
+});
+
+string connectionString = "Data Source=app.db";
+builder.Services.AddScoped<IDbConnection>(sp => new SqliteConnection(connectionString));
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddSingleton<JwtService>();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+using (var connection = new SqliteConnection(connectionString))
+{
+    connection.Open();
+    var sql = connection.CreateCommand();
+    sql.CommandText = @"CREATE TABLE IF NOT EXISTS Users (Id INTEGER PRIMARY KEY AUTOINCREMENT, Username TEXT NOT NULL UNIQUE, PasswordHash TEXT NOT NULL);";
+    sql.ExecuteNonQuery();
+    Console.WriteLine("DB Table created");
+}
+
+    // Configure the HTTP request pipeline.
+
+    app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
