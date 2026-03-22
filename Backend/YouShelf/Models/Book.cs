e713fe40f736186bd6fa657bc6e46ea20cd14681
joko675 <kamil100300@gmail.com>
2026-03-22 20:20:55 +0100

    test commit

--- Backend/YouShelf/Models/Book.cs
+++ Backend/YouShelf/Models/Book.cs
@@ -4,7 +4,7 @@ public class Book
 {
     public string Title { get; set; }
     public string Description  { get; set; }
-    public DateTime ReleaseDate { get; set; }
+    public DateOnly ReleaseDate { get; set; }
     public string ImageUrl { get; set; }
     public string Review { get; set; }
 }
