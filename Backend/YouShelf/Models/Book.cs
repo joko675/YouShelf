181f4ef2ba002b271f24cf1723af9efa8b6097d2
joko675 <128475080+joko675@users.noreply.github.com>
2026-03-22 12:57:13 +0100

    Create Book.cs

---
+++ Backend/YouShelf/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace YouShelf.Models;
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Description  { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public string ImageUrl { get; set; }
+    public string Review { get; set; }
+}
